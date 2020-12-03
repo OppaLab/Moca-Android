277deb5bcab4359285406e94b35a2e8283da9264
hk <khman119@gmail.com>
2020-12-03 15:19:10 +0900

    체크버튼 및 좋아요 버튼 벡터파일로 변환

--- app/src/main/java/com/oppalab/moca/util/RetrofitConnection.kt
+++ app/src/main/java/com/oppalab/moca/util/RetrofitConnection.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitConnection {
-    val URL = "http://172.30.1.15:8080"
+    val URL = "http://172.30.1.41:8080"
     val retrofit = Retrofit.Builder().baseUrl(URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
